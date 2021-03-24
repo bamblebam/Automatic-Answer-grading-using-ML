1ba8c626152bce1a059f5e0c6677743fcf574474
kush-pixel <patel.h.kush@gmail.com>
2021-03-24 11:55:14 +0530

    cleraed all comments

--- miniproject/quiz/models.py
+++ miniproject/quiz/models.py
@@ -8,29 +8,17 @@ class Question(models.Model):
     author = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
     title = models.CharField(max_length=200, null=False)
     model_answer = models.TextField(null=True)
-    # created_at = models.DateTimeField(auto_now_add=True)
-    # updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
-
-    # def get_responses(self):
-    #     return self.responses.filter(parent=None)
-
 
 class Response(models.Model):
     user = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
     question = models.ForeignKey(
         Question, null=False, on_delete=models.CASCADE, related_name='responses')
-    # parent = models.ForeignKey(
-    #     'self', blank=True, on_delete=models.CASCADE)
     body = models.TextField(null=False)
-    # created_at = models.DateTimeField(auto_now_add=True)
-    # updated_at = models.DateTimeField(auto_now=True)
-
+ 
     def __str__(self):
         return self.body
 
-    # def get_responses(self):
-    #     return Response.objects.filter(parent=self)
-# Created,updated and login, register URL should be removed and parent part should be removed.
+
