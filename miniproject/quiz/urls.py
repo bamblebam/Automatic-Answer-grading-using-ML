46686eff5b625f1a71e6751c266644a8febf34a7
bamblebam <burhanizrangwala@gmail.com>
2021-03-29 20:37:19 +0530

    dashboard page progress

--- miniproject/quiz/urls.py
+++ miniproject/quiz/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("new-question", views.newQuestionPage, name='new-question'),
-    path("", views.QuestionListView.as_view(), name='question-home'),
+    path("", views.QuestionListView.as_view(), name='quiz-home'),
     path('question/<slug>', views.questionPage, name='question'),
 ]
