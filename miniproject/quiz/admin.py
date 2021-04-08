da8d5d7544c3efd64785a312cbfa9683aa4a72e7
bamblebam <burhanizrangwala@gmail.com>
2021-04-08 23:48:52 +0530

    models updated

--- miniproject/quiz/admin.py
+++ miniproject/quiz/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from.models import Question, Response
+from.models import Question, Response, Exam, ExamResponse
 # Register your models here.
 admin.site.register(Question)
 admin.site.register(Response)
+admin.site.register(Exam)
+admin.site.register(ExamResponse)
--- miniproject/quiz/models.py
+++ miniproject/quiz/models.py
@@ -11,8 +11,9 @@ class Question(models.Model):
     title = models.CharField(max_length=200)
     model_answer = models.TextField(null=True)
     question_code = models.CharField(max_length=200, default=uuid4().hex[:6])
-    slug = models.SlugField(max_length=255, null=True)
+    slug = models.SlugField(max_length=255, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    is_exam = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
@@ -30,8 +31,9 @@ class Response(models.Model):
     body = models.TextField(null=False)
     marks = models.IntegerField(default=0, validators=[
                                 MinValueValidator(0), MaxValueValidator(100)])
-    slug = models.SlugField(max_length=255, null=True)
+    slug = models.SlugField(max_length=255, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    is_exam = models.BooleanField(default=False)
 
     def __str__(self):
         return self.body
@@ -40,3 +42,39 @@ class Response(models.Model):
         if not self.slug:
             self.slug = slugify(uuid4().hex[:10])
         super(Response, self).save(*args, **kwargs)
+
+
+class Exam(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    questions = models.ManyToManyField(Question, symmetrical=False)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    exam_code = models.CharField(
+        max_length=200, default=uuid4().hex[:6], null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(uuid4().hex[:10])
+        super(Exam, self).save(*args, **kwargs)
+
+
+class ExamResponse(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    responses = models.ManyToManyField(Response, symmetrical=False)
+    slug = models.SlugField(max_length=200, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    marks = models.IntegerField(default=0, validators=[
+        MinValueValidator(0), MaxValueValidator(100)])
+
+    def __str__(self):
+        return self.exam.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(uuid4().hex[:10])
+        super(ExamResponse, self).save(*args, **kwargs)
